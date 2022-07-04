0eaf1265b03cda66fc05f5f547213b26edc99f04
GadMinz <baykov97@mail.ru>
2022-07-04 21:00:16 +0300

    Add No results

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Card from "../components/Card";
 import { useSelector } from "react-redux";
+import noResults from "../assets/img/no-results.svg";
 
 const Main = () => {
   const { items, totalItems, status } = useSelector((state) => state.book);
@@ -10,9 +11,18 @@ const Main = () => {
         {status === "loading" ? "Searching..." : `Found ${totalItems} results`}
       </div>
       <div className="main__cards">
-        {status === "loading"
-          ? ""
-          : items.map((obj) => <Card key={obj.id} {...obj.volumeInfo} />)}
+        {status === "loading" ? (
+          ""
+        ) : totalItems > 0 ? (
+          items.map((obj) => <Card key={obj.id} {...obj.volumeInfo} />)
+        ) : (
+          <div className="no-results">
+            <div className="no-results--img">
+              <img width={200} src={noResults} alt="noResults" />
+            </div>
+            <div className="no-results--title">No results</div>
+          </div>
+        )}
       </div>
     </div>
   );
