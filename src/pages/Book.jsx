ace1ae8fff579624bf8db63ff9f94fc0cdedbb26
GadMinz <baykov97@mail.ru>
2022-07-06 16:36:11 +0300

    Add Loading in Book page

--- src/pages/Book.jsx
+++ src/pages/Book.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 import axios from "axios";
+import bookNotImg from "../assets/img/error-book.png";
+import spinner from "../assets/img/spinner.svg";
 
 const Book = () => {
   const [book, setBook] = React.useState({
@@ -9,6 +11,8 @@ const Book = () => {
     authors: [],
     description: "",
   });
+  const [isLoading, setIsLoading] = React.useState(true);
+
   React.useEffect(() => {
     const bookId = window.location.pathname.split("/").pop();
     const fetchBook = async () => {
@@ -17,8 +21,13 @@ const Book = () => {
           `https://www.googleapis.com/books/v1/volumes/${bookId}`
         );
         const info = data.volumeInfo;
+        const bookImg = info.imageLinks
+          ? info.imageLinks.extraLarge ||
+            info.imageLinks.large ||
+            info.imageLinks.medium
+          : bookNotImg;
         setBook({
-          image: info.imageLinks.extraLarge,
+          image: bookImg,
           categories: info.categories,
           title: info.title,
           authors: info.authors,
@@ -27,32 +36,40 @@ const Book = () => {
       } catch (e) {
         alert("Ошибка при запросе данных");
         console.error(e);
+      } finally {
+        setIsLoading(false);
       }
     };
     fetchBook();
   }, []);
 
   return (
-    <div className="book">
-      <div className="book__image">
-        <img src={book.image} alt="book-img" />
-      </div>
-      <div className="book__info">
-        <div className="book__info-categories">
-          {book.categories ? book.categories.join(" / ") : <br />}
-        </div>
-        <div className="book__info-title">
-          {book.title ? book.title : <br />}
-        </div>
-        <div className="book__info-authors">
-          {book.authors ? book.authors.join(", ") : <br />}
-        </div>
+    <>
+      {isLoading ? (
+        <img width={300} className="spinner" src={spinner} alt="" />
+      ) : (
+        <div className="book">
+          <div className="book__image">
+            <img src={book.image} alt="book-img" />
+          </div>
+          <div className="book__info">
+            <div className="book__info-categories">
+              {book.categories ? book.categories.join(" / ") : <br />}
+            </div>
+            <div className="book__info-title">
+              {book.title ? book.title : <br />}
+            </div>
+            <div className="book__info-authors">
+              {book.authors ? book.authors.join(", ") : <br />}
+            </div>
 
-        {book.description && (
-          <div className="book__info-description">{book.description}</div>
-        )}
-      </div>
-    </div>
+            {book.description && (
+              <div className="book__info-description">{book.description}</div>
+            )}
+          </div>
+        </div>
+      )}
+    </>
   );
 };
 
